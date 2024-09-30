41d632ac16b167b987d1ae890d80620b2b1f64dc
Omer Dayan <omerdayan94@gmail.com>
2024-09-30 09:38:53 +0300

    Creation of Water Interface Management

---
+++ WaterResource.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef WATERRESOURCE_H
+#define WATERRESOURCE_H
+
+#include"Resource.h"
+
+class WaterResource : public Resource {
+public:
+    WaterResource() : Resource("Water") {}
+};
+
+
+#endif // !WATERRESOURCE_H

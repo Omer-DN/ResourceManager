ea730cf82448e2c2a8fef13ecd131d6e03e4af9e
Omer Dayan <omerdayan94@gmail.com>
2024-09-30 09:39:16 +0300

    Creation of Internet Interface Management

---
+++ InternetResource.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef INTERNETRESOURCE_H
+#define INTERNETRESOURCE_H
+
+#include"Resource.h"
+
+class InternetResource:public Resource
+{
+public:
+	InternetResource() : Resource("internet") {};
+
+
+};
+
+#endif // !INTERNETRESOURCE_H
+

70dc0c9acf4d4a5ad3d2bb953e0001508522eb77
Omer Dayan <omerdayan94@gmail.com>
2024-09-30 10:49:26 +0300

    Adding mutex lock var

--- Water.h
+++ Water.h
@@ -3,12 +3,19 @@
 #define WATER_H
 
 #include"Resource.h"
+#include <mutex>
 
 class Water : public Resource {
 public:
     Water(double amount);
+
     void useResource(double amount) override;
     void displayResource() override;
+    void replenishResource(double amount);
+
+
+private:
+    mutex mtx;
 };
 
 

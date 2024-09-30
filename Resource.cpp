68374494fa34fdcea4a11c07fb3bf4fd5805f3ca
Omer Dayan <omerdayan94@gmail.com>
2024-09-30 10:01:25 +0300

    Adding function to this class

--- Resource.cpp
+++ Resource.cpp
@@ -1,14 +1,31 @@
 #include"Resource.h"
 
-void Resource::updateUsage(double newUsage) {
-	this->usage += newUsage;
-	cout << "now usage of " << resourceName << " is: " << newUsage << "\n";
+Resource::Resource(const string& name, double amount)
+	:resourceName(name), resourceAmount(amount){}
+
+Resource::~Resource(){}
+
+void Resource::replenishResource(double amount) {
+	resourceAmount += amount;
 }
 
+void Resource::updateUsage(double amount) {
+	resourceAmount = amount;
+}
+
+string Resource::getResourceName() const {
+	return resourceName;
+}
+
+double Resource::getResourceAmount() const {
+	return resourceAmount;
+}
+
+
 double Resource::getUsage() {
-	return usage;
+	return resourceAmount;
 }
 
 void Resource::displayResource() {
-	cout << "Resource: " << resourceName << " his usage is: " << usage << "\n";
+	cout << "Resource: " << resourceName << " his usage is: " << resourceAmount << "\n";
 }

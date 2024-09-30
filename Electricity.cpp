733170ef3fd3d31b7ef8601f0d81a225ba08de91
Omer Dayan <omerdayan94@gmail.com>
2024-09-30 10:47:44 +0300

    Using mtx lock

--- Electricity.cpp
+++ Electricity.cpp
@@ -5,15 +5,25 @@ Electricity ::Electricity(double amount)
 	:Resource("Electricity",amount){}
 
 void Electricity::useResource(double amount) {
-	if (resourceAmount>amount){
-        updateUsage(amount);  // שימוש בפונקציה לעדכון השימוש
-        cout << "Used " << amount << " units of electricity. Remaining: " << getUsage() << endl;
+    lock_guard<mutex> lock(mtx); 
+    if (amount <= getUsage()) {
+        double newUsage = getUsage() - amount;
+        updateUsage(newUsage);
+        cout << "Used " << amount << " units of electricity.\n";
     }
     else {
-        cout << "Not enough electricity available!" << endl;
+        cout << "Not enough electricity available!\n";
     }
 }
 
+void Electricity::replenishResource(double amount) {
+    lock_guard<mutex> lock(mtx); 
+    double newAmount = getUsage() + amount;
+    updateUsage(newAmount);
+    cout << "Replenished " << amount << " units of electricity.\n";
+}
+
 void Electricity::displayResource(){
-    cout << "Electricity Resource - Amount: " << getUsage() << endl;
+    lock_guard<mutex> lock(mtx);
+    cout << "Electricity usage: " << getUsage() << " units.\n";
 }

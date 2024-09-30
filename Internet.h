905e01f5aca97c6278b33ac2898829aba9fdce3f
Omer Dayan <omerdayan94@gmail.com>
2024-09-30 10:54:05 +0300

    Adding mutex lock var

--- Internet.h
+++ Internet.h
@@ -3,15 +3,20 @@
 #define INTERNE_H
 
 #include"Resource.h"
+#include <mutex>
+
 
 class Internet :public Resource
 {
 public:
 	Internet(double amount);
+
 	void useResource(double amount) override;
 	void displayResource() override;
+	void replenishResource(double amount);
 
-   
+private:
+	mutex mtx;
 };
 
 #endif // !INTERNET_H

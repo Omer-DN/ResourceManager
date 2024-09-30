05e7d4cb13ee9b879ced64148f820f0eb7cc2428
Omer Dayan <omerdayan94@gmail.com>
2024-09-30 13:46:15 +0300

    Fixing the code

--- Resource.h
+++ Resource.h
@@ -2,19 +2,29 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
 
-#include<iostream>
-#include<string>
-
+#include <iostream>
+#include <string>
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
 using namespace std;
 
 class Resource
 {
 protected:
 	string resourceName;
-	double usage;
+	double resourceAmount;
+	mutex resourceMutex;
 
 public:
-	Resource(string name) : resourceName(name), usage(0) {};
+	Resource(const string& name, double amount);
+	virtual ~Resource();
+	string getResourceName() const;
+	double getResourceAmount()const;
+
+	virtual void useResource(double amount) = 0;
+	virtual void replenishResource(double amount);
 
 	virtual void updateUsage(double amount);
 	virtual double getUsage();

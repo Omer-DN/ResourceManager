3513310e56c1fe99ea6ef6cbf66d7be2fde13fc0
Omer Dayan <omerdayan94@gmail.com>
2024-09-30 11:02:22 +0300

    Using Threads in the Main File

--- main.cpp
+++ main.cpp
@@ -1,90 +1,52 @@
 #include <iostream>
+#include <thread>
 #include "Electricity.h"
 #include "Water.h"
 #include "Internet.h"
 
 using namespace std;
 
-void displayMenu() {
-    cout << "\nResource Management System\n";
-    cout << "1. Use Electricity\n";
-    cout << "2. Use Water\n";
-    cout << "3. Use Internet\n";
-    cout << "4. Replenish Electricity\n";
-    cout << "5. Replenish Water\n";
-    cout << "6. Replenish Internet\n";
-    cout << "7. Display Resources\n";
-    cout << "8. Exit\n";
-    cout << "Choose an option: ";
+// פונקציה שתשתמש במשאב חשמל
+void useElectricity(Electricity& electricityResource, double amount) {
+    electricityResource.useResource(amount);
+}
+
+// פונקציה שתשתמש במשאב מים
+void useWater(Water& waterResource, double amount) {
+    waterResource.useResource(amount);
+}
+
+// פונקציה שתשתמש במשאב אינטרנט
+void useInternet(Internet& internetResource, double amount) {
+    internetResource.useResource(amount);
 }
 
 int main() {
+    // יצירת משאבים
+    Electricity electricity(100); // 100 יחידות חשמל
+    Water water(50);               // 50 יחידות מים
+    Internet internet(75);         // 75 יחידות אינטרנט
 
-    Electricity electricity(100);
-    Water water(200);
-    Internet internet(500);
+    // יצירת תהליכים
+    thread t1(useElectricity, ref(electricity), 20);
+    thread t2(useWater, ref(water), 10);
+    thread t3(useInternet, ref(internet), 30);
+    thread t4(useElectricity, ref(electricity), 15);
+    thread t5(useWater, ref(water), 5);
+    thread t6(useInternet, ref(internet), 20);
 
-    int choice;
-    double amount;
+    // המתנה לסיום כל התהליכים
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    t5.join();
+    t6.join();
 
-    while (true) {
-        displayMenu();
-        cin >> choice;
-
-        switch (choice) {
-        case 1:
-            cout << "Enter amount of electricity to use: ";
-            cin >> amount;
-            electricity.useResource(amount);
-            break;
-
-        case 2:
-            cout << "Enter amount of water to use: ";
-            cin >> amount;
-            water.useResource(amount);
-            break;
-
-        case 3:
-            cout << "Enter amount of internet (MB) to use: ";
-            cin >> amount;
-            internet.useResource(amount);
-            break;
-
-        case 4:
-            cout << "Enter amount of electricity to replenish: ";
-            cin >> amount;
-            electricity.replenishResource(amount);
-            cout << "Electricity replenished.\n";
-            break;
-
-        case 5:
-            cout << "Enter amount of water to replenish: ";
-            cin >> amount;
-            water.replenishResource(amount);
-            cout << "Water replenished.\n";
-            break;
-
-        case 6:
-            cout << "Enter amount of internet (MB) to replenish: ";
-            cin >> amount;
-            internet.replenishResource(amount);
-            cout << "Internet replenished.\n";
-            break;
-
-        case 7:
-            electricity.displayResource();
-            water.displayResource();
-            internet.displayResource();
-            break;
-
-        case 8:
-            cout << "Exiting...\n";
-            return 0;
-
-        default:
-            cout << "Invalid choice. Please try again.\n";
-        }
-    }
+    // הצגת סטטוס המשאבים
+    electricity.displayResource();
+    water.displayResource();
+    internet.displayResource();
 
     return 0;
 }

872ea7e94efc6d32f1f2c09d751e6ea20f4ce73b
Omer Dayan <omerdayan94@gmail.com>
2024-09-30 10:47:25 +0300

    Adding mutex lock var

--- Electricity.h
+++ Electricity.h
@@ -10,8 +10,10 @@ class Electricity:public Resource
 {
 public:
 	Electricity(double amount);
+
 	void useResource(double amount) override;
 	void displayResource() override;
+	void replenishResource(double amount);
 
 private:
 	mutex mtx;
